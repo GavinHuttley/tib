20a095e42a049c162c5e04dabcaebb6534dc8070
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2021-07-15 14:44:34 +1000

    MAINT: automatically update copyright and release by the build date

--- source/conf.py
+++ source/conf.py
@@ -1,12 +1,15 @@
+import datetime
 import os
+
+today = datetime.date.today()
+
 os.environ["COGENT3_WARNINGS"] = "ignore"
 os.environ["COGENT3_ALIGNMENT_REPR_POLICY"] = "wrap=30,num_pos=30"
 
 project = "Topics in Bioinformatics"
-copyright = "2020, Gavin Huttley"
+copyright = f"2020-{today:%Y}, Gavin Huttley"
+release = f"{today:%Y.%m.%d}"
 author = "Gavin Huttley"
-
-release = "2020"
 
 
 # -- General configuration ---------------------------------------------------
@@ -15,7 +18,7 @@ rst_prolog = """
 .. include:: <s5defs.txt>
 .. include:: /_globals.rst
 """
- 
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.graphviz",
@@ -33,7 +36,7 @@ extensions = [
 
 # todo_include_todos = True
 show_authors = True
-graphviz_output_format = 'svg'
+graphviz_output_format = "svg"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -47,29 +50,29 @@ html_theme = "sphinx_rtd_theme"
 # -- Options for HTML output -------------------------------------------------
 
 html_context = {
-    "display_github": True, # Integrate GitHub
-    "github_user": "GavinHuttley", # Username
-    "github_repo": "tib", # Repo name
-    "github_version": "develop", # Version
-    "conf_py_path": "/source/", # Path in the checkout to the docs root
+    "display_github": True,  # Integrate GitHub
+    "github_user": "GavinHuttley",  # Username
+    "github_repo": "tib",  # Repo name
+    "github_version": "develop",  # Version
+    "conf_py_path": "/source/",  # Path in the checkout to the docs root
 }
 
 html_static_path = ["_static"]
 
 html_theme_options = {
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links": False,
     # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 6,
-    'includehidden': True,
-    'titles_only': False,
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 6,
+    "includehidden": True,
+    "titles_only": False,
 }
 
 html_css_files = [
-    'css/custom.css',
+    "css/custom.css",
 ]
 
 bibtex_bibfiles = ["references.bib"]
